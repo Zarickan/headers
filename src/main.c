60695018740e154573fb8720258e502d8de38126
Frederik Munk Madsen <frederik@os-madsen.dk>
2018-01-28 12:40:56 +0100

    Auto-sizing stack (of pointers)

--- src/main.c
+++ src/main.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
 #include "types.h"
+#include "stack.h"
 
 int main(int argc, char** argv)
 {
+    Stack stack;
+    stack_init(&stack, 4);
+    
+    v3 vec1, vec2, vec3, vec4, vec5;
+    
+    vec1.x = 10;
+    vec1.y = 20;
+    vec1.z = 30;
+    
+    vec2.x = 1;
+    vec2.y = 2;
+    vec2.z = 3;
+    
+    vec3.x = 0.123f;
+    vec3.y = 0.321f;
+    vec3.z = 0.111f;
+    
+    vec5.x = 1.234f;
+    vec5.y = 2.234f;
+    vec5.z = 3.234f;
+    
+    stack_push(&stack, &vec1);
+    stack_push(&stack, &vec2);
+    stack_push(&stack, &vec3);
+    stack_push(&stack, &vec4);
+    stack_push(&stack, &vec5);
+    
+    for(u8 i = 0; i < 4; i++) {
+        v3* el = (v3*) stack_pop(&stack);
+        printf("Element %i from top is: {%f, %f, %f} \n", i, el->x, el->y, el->z);
+    }
+    
+    v3* peek = (v3*) stack_peek(&stack);
+    printf("Peeking at {%f, %f, %f}\n", peek->x, peek->y, peek->z);
+    
     printf("Hello world!\n");
     return 0;
 }
---
+++ src/stack.h
@@ -0,0 +1,73 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <assert.h>
+#include "types.h"
+
+#ifndef STACK_H
+#define STACK_H
+
+typedef struct Stack {
+    u32 size;
+    u32 position;
+    u32 capacity;
+    
+    void** buffer;
+} Stack;
+
+inline void
+stack_init(Stack* stack, u32 size) {
+    stack->buffer = (void*) malloc(sizeof(void*) * size);
+    
+    memset(stack, 0, sizeof(u32) * 2); // NOTE (Frederik): Sets first 2 fields to 0
+    stack->capacity = size;
+    
+}
+
+inline Stack*
+stack_create(u32 size) { 
+    Stack* stack = (Stack*) malloc(sizeof(Stack));
+    stack_init(stack, size);
+    
+    return stack;
+}
+
+inline void
+stack_push(Stack* stack, void* element) {
+    if(stack->position > stack->capacity - 1) {
+        assert(stack->capacity * 2 <= UINT32_MAX);
+        
+        u32 capacity = stack->capacity * 2;
+        stack->buffer = realloc(stack->buffer, sizeof(void*) * capacity);
+        stack->capacity = capacity;
+    }
+    
+    stack->buffer[stack->position] = element;
+    stack->size++;
+    stack->position++;
+}
+
+
+inline void*
+stack_pop(Stack* stack) {
+    assert(stack->position >= 1);
+    
+    stack->size--;
+    stack->position--;
+    void* element = stack->buffer[stack->position];
+    stack->buffer[stack->position] = NULL;
+    
+    u32 capacity = stack->capacity / 4;
+    if(stack->position < capacity && capacity > 0) {
+        stack->buffer = realloc(stack->buffer, sizeof(void*) * capacity);
+        stack->capacity = capacity;
+    }
+    
+    return element;
+}
+
+inline void*
+stack_peek(Stack* stack) {
+    return stack->buffer[stack->position - 1];
+}
+
+#endif // STACK_H
