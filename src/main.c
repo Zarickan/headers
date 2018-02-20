dcd613d5675324d76fb102d5bef280ff5ee73012
Frederik Munk Madsen <frederik@os-madsen.dk>
2018-02-20 21:20:18 +0100

    Fixed prinfting of hex values for tests in main.c

--- src/main.c
+++ src/main.c
@@ -16,22 +16,22 @@ int main(int argc, char** argv)
     ico_read_header(&header, file);
     
     printf("Cursor header:\n");
-    printf("  reserved:   0x%#u\n", (u16) (header.reserved[0] + header.reserved[1]));
-    printf("  imageType:  0x%#u\n", header.imageType);
-    printf("  imageCount: 0x%#u\n", header.imageCount);
+    printf("  reserved:   0x%x\n", (u16) (header.reserved[0] + header.reserved[1]));
+    printf("  imageType:  0x%x\n", header.imageType);
+    printf("  imageCount: 0x%x\n", header.imageCount);
     
     IcoEntry* entries = ico_read_entries(&header, file);
     for(u16 i = 0; i < header.imageCount; i++) {
         IcoEntry entry = entries[i];
         printf("Cursor entry:\n");
-        printf("  width:        0x%#u\n", entry.width);
-        printf("  height:       0x%#u\n", entry.height);
-        printf("  paletteSize:  0x%#u\n", entry.paletteSize);
-        printf("  reserved:     0x%#u\n", entry.reserved);
-        printf("  colorplanes:  0x%#u\n", entry.colorPlanes);
-        printf("  bitsPerPixel: 0x%#u\n", entry.bitsPerPixel);
-        printf("  size:         0x%#u\n", entry.size);
-        printf("  offset:       0x%#u\n", entry.offset);
+        printf("  width:        0x%x\n", entry.width);
+        printf("  height:       0x%x\n", entry.height);
+        printf("  paletteSize:  0x%x\n", entry.paletteSize);
+        printf("  reserved:     0x%x\n", entry.reserved);
+        printf("  colorplanes:  0x%x\n", entry.colorPlanes);
+        printf("  bitsPerPixel: 0x%x\n", entry.bitsPerPixel);
+        printf("  size:         0x%x\n", entry.size);
+        printf("  offset:       0x%x\n", entry.offset);
     }
     
     fclose(file);
