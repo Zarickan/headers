0d026ea003e2e444ab2066454760aa1b7768f7af
Frederik Munk Madsen <frederik@os-madsen.dk>
2018-03-07 12:35:02 +0100

    Updated ico.h format from icotool

--- src/formats/ico.h
+++ src/formats/ico.h
@@ -2,11 +2,15 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <assert.h>
-#include "../types.h"
+#include "types.h"
 
 #ifndef ICO_H
 #define ICO_H
 
+#define TYPE_ICO 1
+#define TYPE_CUR 2
+
+#pragma pack(push, 1)
 typedef struct IcoHeader {
     u08 reserved[2];
     
@@ -20,7 +24,7 @@ typedef struct IcoEntry {
     
     u08 paletteSize;
     
-    u08 reserved; // NOTE: Should always be 0
+    u08 reserved;
     
     u16 colorPlanes;
     u16 bitsPerPixel;
@@ -28,23 +32,29 @@ typedef struct IcoEntry {
     u32 size;
     u32 offset;
 } IcoEntry;
+#pragma pack(pop)
 
 static inline void
 ico_read_header(IcoHeader* header, FILE* file) {
-    if(header == NULL) {
+    if (header == NULL) {
         header = malloc(sizeof(IcoHeader));
     }
     
     fread(header, sizeof(IcoHeader), 1, file);
+    printf("Reserved: %u %u\n", header->reserved[0], header->reserved[1]);
+    assert(header->reserved[0] == 0 &&  header->reserved[1] == 0);
+    assert(header->imageType == 1 || header->imageType == 2);
 }
 
 static inline IcoEntry*
 ico_read_entries(IcoHeader* header, FILE* file) {
     IcoEntry* entries = malloc(sizeof(IcoEntry) * header->imageCount);
-    
-    for(u16 i = 0; i < header->imageCount; i++) {
-        IcoEntry* entry = &entries[i];
-        fread(entry, sizeof(IcoEntry), 1, file);
+    fread(entries, sizeof(IcoEntry), header->imageCount, file);
+    for (u08 i = 0; i < header->imageCount; i++) {
+        assert(entries[i].reserved == 0);
+        
+        if (header->imageType == TYPE_ICO)
+            assert(entries[i].colorPlanes < 2);
     }
     
     return entries;
@@ -58,13 +68,16 @@ ico_read(IcoHeader* header, FILE* file) {
 
 static inline void
 ico_write_header(IcoHeader* header, FILE* file) {
+    assert(header->reserved[0] == header->reserved[1] == 0);
     fwrite(header, sizeof(IcoHeader), 1, file);
 }
 
 static inline void
 ico_write_entries(IcoHeader* header, IcoEntry* entries, FILE* file) {
-    for(u16 i = 0; i < header->imageCount; i++) {
+    for (u16 i = 0; i < header->imageCount; i++) {
         IcoEntry* entry = &entries[i];
+        
+        assert(entry->reserved == 0);
         fwrite(entry, sizeof(IcoEntry), 1, file);
     }
 }
