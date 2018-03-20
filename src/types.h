eae4323016dee74897d43a4e7f12788a86e2eff4
Frederik Munk Madsen <frederik@os-madsen.dk>
2018-03-20 21:18:36 +0100

    f32 x4 SIMD functions

--- src/types.h
+++ src/types.h
@@ -111,4 +111,140 @@ v2_normalize(v2 vector) {
     vector.y *= magnitude;
 }
 
+#if defined(_MSC_VER)
+#include <intrin.h>
+#elif defined(__GNUC__) && (defined(__x86_64__) || defined(__i386__))
+#include <x86intrin.h>
+#endif
+
+typedef struct f32_4x {
+    union {
+        __m128 P;
+        f32    E[4];
+        u32    U[4];
+    };
+} f32_4x;
+
+#define MM_CVT_U32_F32
+
+inline static f32_4x
+f32_set(f32 f0, f32 f1, f32 f2, f32 f3) {
+    f32_4x result;
+    result.P = _mm_set_ps(f3, f2, f1, f0);
+    
+    return result;
+}
+
+inline static f32_4x
+f32_set_all(f32 all) {
+    f32_4x result;
+    result.P = _mm_set1_ps(all);
+    
+    return result;
+}
+
+inline static f32_4x
+f32_zero() {
+    f32_4x result;
+    result.P = _mm_setzero_ps();
+    
+    return result;
+}
+
+inline static f32_4x
+f32_add(f32_4x left, f32_4x right) {
+    f32_4x result;
+    result.P = _mm_add_ps(left.P, right.P);
+    
+    return result;
+}
+
+inline static f32_4x
+f32_sub(f32_4x left, f32_4x right) {
+    f32_4x result;
+    result.P = _mm_sub_ps(left.P, right.P);
+    
+    return result;
+}
+
+inline static f32_4x
+f32_mul(f32_4x left, f32_4x right) {
+    f32_4x result;
+    result.P = _mm_mul_ps(left.P, right.P);
+    
+    return result;
+}
+
+inline static f32_4x
+f32_div(f32_4x left, f32_4x right) {
+    f32_4x result;
+    result.P = _mm_div_ps(left.P, right.P);
+    
+    return result;
+}
+
+inline static f32_4x
+f32_xor(f32_4x left, f32_4x right) {
+    f32_4x result;
+    result.P = _mm_xor_ps(left.P, right.P);
+    
+    return result;
+}
+
+inline static f32_4x
+f32_or(f32_4x left, f32_4x right) {
+    f32_4x result;
+    result.P = _mm_or_ps(left.P, right.P);
+    
+    return result;
+}
+
+inline static f32_4x
+f32_and(f32_4x left, f32_4x right) {
+    f32_4x result;
+    result.P = _mm_and_ps(left.P, right.P);
+    
+    return result;
+}
+
+inline static f32_4x
+f32_lss(f32_4x left, f32_4x right) {
+    f32_4x result;
+    result.P = _mm_cmplt_ps(left.P, right.P);
+    
+    return result;
+}
+
+inline static f32_4x
+f32_leq(f32_4x left, f32_4x right) {
+    f32_4x result;
+    result.P = _mm_cmple_ps(left.P, right.P);
+    
+    return result;
+}
+
+inline static f32_4x
+f32_gtr(f32_4x left, f32_4x right) {
+    f32_4x result;
+    result.P = _mm_cmpge_ps(left.P, right.P);
+    
+    return result;
+}
+
+inline static f32_4x
+f32_eql(f32_4x left, f32_4x right) {
+    f32_4x result;
+    result.P = _mm_cmpeq_ps(left.P, right.P);
+    
+    return result;
+}
+
+inline static f32_4x
+f32_new(f32_4x left, f32_4x right) {
+    f32_4x result;
+    result.P = _mm_cmpneq_ps(left.P, right.P);
+    
+    return result;
+}
+
 #endif // TYPES_H
